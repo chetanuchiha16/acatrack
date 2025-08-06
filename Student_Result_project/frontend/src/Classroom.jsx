544c017f904566c3745b6bb35a8dfb1cc5d09a6d
chetanuchiha16 <chetan16ck@gmail.com>
2025-08-06 22:34:09 +0530

    Add Classroom component, refactor Result and Student components, and update CSS for consistency

--- Student_Result_project/frontend/src/Classroom.jsx
+++ Student_Result_project/frontend/src/Classroom.jsx
@@ -1,11 +1,9 @@
-import { use, useEffect, useState } from "react";
+import React from "react";
 
-export default function Result() {
-    useEffect(() => {}, []);
-
+export default function Classroom() {
     return (
         <div className="dark:text-white dark:bg-[#1a1a1a] text-black w-[80vw] h-[70vh] flex justify-center backdrop-blur-sm items-center border-4 border-black">
-            <h1>Result</h1>
+            <h1>Classroom</h1>
         </div>
-    );
+    )
 }
--- Student_Result_project/frontend/src/Student.jsx
+++ Student_Result_project/frontend/src/Student.jsx
@@ -1,20 +1,65 @@
-import { useLocation } from "react-router-dom"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Result from "./Result";
+import Classroom from "./Classroom";
 export default function Student() {
-    let {who,id, name} = useLocation().state || {}
-    let navigate = useNavigate()
-    
-    useEffect(() => {
-        
+    let { who, id, name } = useLocation().state || {};
+    // let navigate = useNavigate()
+    let [selectedTab, setSelectedTab] = useState("result");
 
-    }, [])
-    return(
-        <div className="flex justify-center items-center m-auto  text-white gap-x-4">
-            <h1 className="absolute top-2 m-auto text-[#1a1a1a]">Welcome {name}</h1>
+    let sems = ["SEM1", "SEM2", "SEM3", "SEM4", "SEM5", "SEM6"];
+    let [currentSem, setCurrentSem] = useState("");
 
-            <button className="rounded" onClick={() => navigate(`/auth/${who}/${id}/result`)}>Result</button>
-            <button className="rounded">Classroom</button>
+    useEffect(() => {}, []);
+    return (
+        <div className="flex justify-center items-center m-auto">
+            <h1 className="absolute top-2 m-auto ">Welcome {name}</h1>
+
+            {/* <button className="rounded" onClick={() => navigate(`/auth/${who}/${id}/result`)}>Result</button> */}
+            <div className="relative">
+                <div className="flex items-center justify-center min-h-[46px]">
+                    <div className=" border-4 border-b-0 border-black w-fit absolute left-0">
+                        <button
+                            className="border border-black"
+                            onClick={() => setSelectedTab((prev) => "result")}
+                        >
+                            Result
+                        </button>
+                        <button
+                            className=""
+                            onClick={() =>
+                                setSelectedTab((prev) => "classroom")
+                            }
+                        >
+                            Classroom
+                        </button>
+                    </div>
+                    <div className="max-w-fit min-h-[46px] justify-self-center border-4 border-b-0 border-black">
+                        <select
+                            className="h-[46px]"
+                            id="current"
+                            value={currentSem}
+                            onChange={(e) => setCurrentSem(e.target.value)}
+                        >
+                            {sems.map((sem, i) => (
+                                <option
+                                    className="dark:bg-[#1a1a1a]"
+                                    key={i}
+                                    value={sem}
+                                    id={i}
+                                >
+                                    {sem}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+                {selectedTab === "result" && <Result />}
+                {selectedTab === "classroom" && <Classroom />}
+            </div>
+
+            {/* <button className="rounded">Classroom</button> */}
         </div>
-    )
+    );
 }
