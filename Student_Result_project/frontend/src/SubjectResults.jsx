79f476c6b02ececae69d87f735f0f8c8e1e03e60
chetanuchiha16 <chetan16ck@gmail.com>
2026-03-07 08:36:40 +0530

    fixed the overall result and subject result crashing

--- Student_Result_project/frontend/src/SubjectResults.jsx
+++ Student_Result_project/frontend/src/SubjectResults.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import API_BASE from "./config";
 import { semesterOptions, subjectMapping } from "./config";
 import { fetchWithAuth } from "./fetchWithAuth";
