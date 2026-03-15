f951af5d320de9ce2fa5c865d183dea4dbadb5ab
chetanuchiha16 <chetan16ck@gmail.com>
2026-03-15 17:14:32 +0530

    typescripted admin login, app, reset password and staffresult

--- Student_Result_project/frontend/src/App.tsx
+++ Student_Result_project/frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import Auth from "./Auth";
-import AdminLogin from "./AdminLogin";
 import HiddenShortcut from "./HiddenShortcut";
 import "./i18n";
 
-function App() {
-    const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
     return (
         <>
             <Auth />
             <HiddenShortcut/>
         </>
     );
-}
+};
 
 export default App;
