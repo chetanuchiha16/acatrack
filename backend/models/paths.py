f22d0eeb1c126ef85e38d23ff984a5427d32b459
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-14 17:38:39 +0530

    refactor(models,utils): migrate models and utilities to pure SQLAlchemy and FastAPI Depends
    - Refactored models to use SQLAlchemy 2.0 declarative base instead of DB instance
    - Utilities changed kwargs to extract elements from FastAPI 'Request' object
    - Created sync_db utility for backward-compatibility in jobs

--- backend/models/paths.py
+++ backend/models/paths.py
@@ -5,15 +5,11 @@ from utils.cloud import download_image_from_url
 
 logger = get_logger(__name__)
 base_dir = Path(__file__).resolve().parent.parent
-# logger.debug("../Inputs")
 excel_path = str(base_dir / "Inputs/ExcelSheet/result list project.xlsx")
 email_excel_path = str(base_dir / "Inputs/ExcelSheet/Email.xlsx")
 mentor_excel_path = str(base_dir / "Inputs/ExcelSheet/Mentor.xlsx")
-# logo_path = str(base_dir / "Inputs" / "Images" / "logo.png")
-# Your Supabase public URL
 logo_url = "https://hpavqkjevepfegkojisn.supabase.co/storage/v1/object/public/uploads/Inputs/Images/logo.png"
 
-# Download and get a local file path lazily
 _cached_logo_path = None
 
 
@@ -24,8 +20,6 @@ def get_logo_path():
     return _cached_logo_path
 
 
-# db_path = str(base_dir / "Outputs" / "student_data.db")
-# db_path = str(base_dir / "instance" / "user.db")
 pdf_dir = str(base_dir / "Outputs" / "PDFs")
 notes_dir = str(base_dir / "Outputs" / "NOTES")
 img_dir = str(base_dir / "Outputs" / "Images")
@@ -39,20 +33,3 @@ def get_excel_path(batch_year: int) -> str:
 
 postgres_db_url = settings.database_url
 API_BASE = "http://localhost:5000"
-# from services.batch_manager import BatchManager
-
-# current_batch_db_path = None
-
-# def set_current_batch_db(batch_year: int):
-#     global current_batch_db_path
-#     current_batch_db_path = BatchManager().get_db_path(batch_year)
-
-# def get_current_db_path():
-#     logger.debug(current_batch_db_path)
-#     return current_batch_db_path
-
-# logger.debug(excel_path)
-# logger.debug(logo_path)
-# logger.debug(base_dir)
-# logger.debug(f"[DEBUG] Using DB path: {base_dir}")
-# logger.debug(f"[DEBUG] Exists? {Path(excel_path).exists()}")
